4d94bdaf9800c09132faf8c539642b8636a81035
Dinesh Gupta <Dineshg0009@gmail.com>
2023-05-15 11:27:24 +0530

    All Services created for Employee

--- src/main/java/com/SpringBootThymeleaf/Service/EmployeeService.java
+++ src/main/java/com/SpringBootThymeleaf/Service/EmployeeService.java
@@ -6,6 +6,9 @@ import java.util.List;
 
 public interface EmployeeService {
 
-    List<Employee> getAllEmployees();
+    List < Employee > getAllEmployees();
+    void saveEmployee(Employee employee);
+    Employee getEmployeeById(long id);
+    void deleteEmployeeById(long id);
 
 }
--- src/main/java/com/SpringBootThymeleaf/Service/EmployeeServiceImpl.java
+++ src/main/java/com/SpringBootThymeleaf/Service/EmployeeServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService{
@@ -16,5 +17,28 @@ public class EmployeeServiceImpl implements EmployeeService{
     @Override
     public List<Employee> getAllEmployees() {
         return employeeRepository.findAll();
+    }
+
+    @Override
+    public void saveEmployee(Employee employee) {
+        this.employeeRepository.save(employee);
+    }
+
+    @Override
+    public Employee getEmployeeById(long id) {
+
+        Optional<Employee> optional = employeeRepository.findById(id);
+        Employee employee = null;
+        if (optional.isPresent())
+            employee=optional.get();
+        else {
+            throw  new RuntimeException("Employee not found for id :" +id );
+        }
+        return employee;
+    }
+
+    @Override
+    public void deleteEmployeeById(long id) {
+        this.employeeRepository.deleteById(id);
     }
 }
